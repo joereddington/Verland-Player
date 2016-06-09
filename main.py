731ae14eb0a6440e4f1e7e75bf2d0b281274b859
paul <paul@darkstar.org>
2016-06-09 10:04:27 -0600

    add font-awesome

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'it worked'
-
+@app.route('/player')
+def player():
+    return render_template('player.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
